77066d9180685f7265bedf73c380b73910ed59eb
Daniel Ke <daniel.shi.ke@outlook.com>
2020-02-04 15:44:54 +0800

    Add <CurrentMonth /> component

---
+++ src/CurrentMonth/index.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import format from 'date-fns/format';
+import styles from './CurrentMonth.scss';
+
+export default class CurrentMonth extends PureComponent {
+  static propTypes = {
+    currentMonth: PropTypes.instanceOf(Date),
+    theme: PropTypes.object,
+  };
+
+  render() {
+    const { currentMonth, theme } = this.props;
+
+    return currentMonth ? (
+      <div
+        className={styles.root}
+        style={{
+          backgroundColor: theme.floatingNav.background,
+          color: theme.floatingNav.color,
+        }}
+      >
+        {format(currentMonth, 'MMM YYYY').toUpperCase()}
+      </div>
+    ) : null;
+  }
+}
