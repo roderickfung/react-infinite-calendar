65edd3ce228c551c7937a0cdd4102916ab28e48d
bneigher <bneigher@gmail.com>
2021-04-22 21:42:11 -0700

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
@@ -5,9 +6,11 @@ import babel from 'rollup-plugin-babel';
 import peer from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import del from 'rollup-plugin-delete';
+import copy from 'rollup-plugin-copy'
 import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
-
+console.log(`${pkg['umd:main']}/${pkg.name}.min.css`);
+console.log("umd/@appannie/react-infinite-calendar.min.css");
 const umdOutputOptions = {
   name: 'InfiniteCalendar',
   format: 'umd',
@@ -61,17 +64,15 @@ export default [
         includeDependencies: true,
       }),
       postcss({
-        minimize: true,
         modules: true,
         use: {
           sass: true,
         }, 
-        extract: true
+        extract: 'styles.css'
       }),
       del({
         targets: pkg.files,
       }),
-      
     ],
   },
   {
@@ -93,13 +94,20 @@ export default [
         sourceMap: true
       }),
       postcss({
-        extract: false,
-        writeDefinitions: true,
-        namedExports: true,
-        use: ['sass'],
+        modules: true,
+        use: {
+          sass: true,
+        }, 
+        extract: true
       }),
       terser({
         
+      }),
+      copy({
+        targets: [
+          { src: `**/*.min.css`, dest: path.resolve('./'), rename: 'styles.css' }
+        ],
+        verbose: true
       }),
     ],
   },
